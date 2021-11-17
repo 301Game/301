8c94a0f2fbfff6dced433ed71fa2dca513ebd508
teddy bear <857719324@qq.com>
2021-11-17 15:25:27 +0800

    角色脚本：防止角色抖动 客厅场景：添加道具碰撞体，完成碰撞测试。

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0.1f;//移动速度
+    Rigidbody2D rigidbody2d;
+    float horizontal;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("接触离开检测");
+        /*
+         * 留用于设置调查图标的消失
+         */
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("碰撞进入检测");
     }
 }
