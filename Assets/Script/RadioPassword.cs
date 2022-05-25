60f3e0c481d04e7ea86038f43c78e7e7657c7b04
zhouyanchen <19301110@bjtu.edu.cn>
2022-05-25 12:03:16 +0800

    放置了按钮，编写了一部分按钮触发

--- Assets/Script/RadioPassword.cs
+++ Assets/Script/RadioPassword.cs
@@ -9,22 +9,70 @@ public class RadioPassword : MonoBehaviour
     private GameObject RadioPanel;
     private GameObject numberPanel;
 
-    private Button[] buttons=new Button[12];
-    private TextMeshProUGUI[] numbers = new TextMeshProUGUI[4];
+    private Button[] numbuttons=new Button[10];
+    private InputField field;
     private string m_targetBlock;
     private string m_booleanVar;
 
     private void Awake()
     {
-        if (buttonNumPanel == null | RadioPanel == null)Debug.LogError("组件不存在");
-        RadioPanel = transform.GetChild(0).gameObject;
-        for (int i = 0; i < 4; i++)
-        {
-            numbers[i] = numberPanel.transform.GetChild(i).GetComponentInChildren<TextMeshProUGUI>();
-            buttons[i] = buttonNumPanel.transform.GetChild(i).GetComponent<Button>();
-        }
+     
     }
-
+    public void clickdelete()
+    {
+        if (field.text.Trim().Length != 0 || !field.text.Equals(""))
+            field.text = field.text.Remove(field.text.Trim().Length - 1, 1);
+    }
+    public void click0()
+    {
+        if(field.text.Trim().Length<=4)
+        field.text += "0";
+    }
+    public void click1()
+    {
+        if (field.text.Trim().Length <= 4)
+            field.text += "1";
+    }
+    public void click2()
+    {
+        if (field.text.Trim().Length <= 4)
+            field.text += "2";
+    }
+    public void click3()
+    {
+        if (field.text.Trim().Length <= 4)
+            field.text += "3";
+    }
+    public void click4()
+    {
+        if (field.text.Trim().Length <= 4)
+            field.text += "4";
+    }
+    public void click5()
+    {
+        if (field.text.Trim().Length <= 4)
+            field.text += "5";
+    }
+    public void click6()
+    {
+        if (field.text.Trim().Length <= 4)
+            field.text += "6";
+    }
+    public void click7()
+    {
+        if (field.text.Trim().Length <= 4)
+            field.text += "7";
+    }
+    public void click8()
+    {
+        if (field.text.Trim().Length <= 4)
+            field.text += "8";
+    }
+    public void click9()
+    {
+        if (field.text.Trim().Length <= 4)
+            field.text += "9";
+    }
     public void Register(string block, string relevant_var)
     {
         m_targetBlock = block;
